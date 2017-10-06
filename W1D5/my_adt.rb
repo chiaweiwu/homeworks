9c992f8338a1f4e3a38fc9b842b1572b228e924f
Chia-Wei Wu <cwwu@umich.edu>
2017-10-06 03:18:42 -0700

    Finished class map with some help

--- W1D5/my_adt.rb
+++ W1D5/my_adt.rb
@@ -42,18 +42,36 @@ class Map
   end
 
   def assign(key, value)
+    # create key if it doesn't exisit yet
+    # if key already exist update value
+    # nested array so to get into each pair we need map[0]
+    pairs_idx = @map.find_index {|pairs| pairs[0] == key} #this would give us the pair idx we need to create keys
+
+    if pairs_idx
+      @map[pairs_idx][key] = value
+    else
+      @map.push([key,value])
+    end
 
   end
 
   def lookup(key)
-
+    @map.each {|pairs| return pairs[1] if pairs[0] == key}
   end
 
   def remove(key)
-
+    @map.delete_if {|pairs| pairs[0] == key}
   end
 
   def show
-
+    @map.dup
   end
 end
+
+
+p map = Map.new
+p map.assign(5,"k")
+p map.assign(3,"k")
+p map.lookup(5)
+p map.remove(5)
+p map.show
